619dec25a22cee721ad31ab9a7e84b21a39a1fbd
fujiyaaa <fujipangesti05@gmail.com>
2025-12-03 21:25:25 +0700

    tabel

--- database/migrations/2025_12_03_134154_create_menus_table.php
+++ database/migrations/2025_12_03_134154_create_menus_table.php
@@ -6,20 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('menus', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id('menu_id');
+            $table->string('name', 100);
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->string('image', 255)->nullable();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('menus');
